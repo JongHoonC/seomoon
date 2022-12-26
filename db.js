279382cdbcc11a6f55adfcede55cd0d498c8a8d5
JongHoonC <bjhlove0126@naver.com>
2022-12-26 15:13:42 +0900

    ejs 회원가입 db 완료

--- db.js
+++ db.js
@@ -3,7 +3,45 @@ var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '1234',
-  database: 'hyosung', //테이블 이름을 적어준다.
+  database: 'seomoon', //테이블 이름을 적어준다.
   dateStrings: 'date',
   multipleStatements: true,
 });
+
+// 회원가입 페이지
+function userSignUp(userName, userID, userPW, callback) {
+  connection.query(`INSERT INTO user_table(userName, userID, userPW, create_time) values('${userName}','${userID}','${userPW}',NOW())`, err => {
+    if (err) throw err;
+    callback();
+  });
+}
+
+// 로그인
+function loginCheck(userID, userPW, callback) {
+  connection.query(`SELECT * FROM user_table WHERE userID='${userID}' and userPW='${userPW}'`, (err, results) => {
+    if (err) throw err;
+    callback(results);
+  });
+}
+// 회원가입 페이지
+function lightuserSignUp(userName, userID, userPW, callback) {
+  connection.query(`INSERT INTO user_table(userName, userID, userPW, create_time) values('${userName}','${userID}','${userPW}',NOW())`, err => {
+    if (err) throw err;
+    callback();
+  });
+}
+
+// 로그인
+function lightloginCheck(userID, userPW, callback) {
+  connection.query(`SELECT * FROM user_table WHERE userID='${userID}' and userPW='${userPW}'`, (err, results) => {
+    if (err) throw err;
+    callback(results);
+  });
+}
+
+module.exports = {
+  userSignUp,
+  loginCheck,
+  lightloginCheck,
+  lightuserSignUp,
+};
--- routes/route.js
+++ routes/route.js
@@ -16,4 +16,68 @@ router.get('/night', (req, res, next) => {
   res.render('nightIndex', {layout: 'nightLayout'});
 });
 
+router.get('/night-sign-in', (req, res, next) => {
+  res.render('night-sign-in', {layout: 'nightLayout'});
+});
+router.get('/night-sign-up', (req, res, next) => {
+  res.render('night-sign-up', {layout: 'nightLayout'});
+});
+router.get('/light-sign-in', (req, res, next) => {
+  res.render('light-sign-in', {layout: 'lightLayout'});
+});
+router.get('/light-sign-up', (req, res, next) => {
+  res.render('light-sign-up', {layout: 'lightLayout'});
+});
+
+// 로그인
+router.post('/night-sign-in', (req, res, next) => {
+  let paramL = JSON.parse(JSON.stringify(req.body));
+  let userID = paramL['userID'];
+  let userPW = paramL['userPW'];
+  db.loginCheck(userID, userPW, result => {
+    if (result.length > 0) {
+      res.redirect('/night');
+    } else {
+      res.send(`<script>alert('로그인정보가 일치하지 않습니다'); document.location.href="/night-sign-in";</script>`);
+    }
+  });
+});
+
+// 회원가입 페이지
+router.post('/night-sign-up', (req, res, next) => {
+  let paramS = JSON.parse(JSON.stringify(req.body));
+  let userName = paramS['userName'];
+  let userID = paramS['userID'];
+  let userPW = paramS['userPW'];
+  db.userSignUp(userName, userID, userPW, () => {
+    res.redirect('/night');
+    //past 에서 특정 값만 넘겨줄 때 row 대신 넘겨줄 값을 써준 후 <% row.id %> 가 아닌 <% id %> 를 써준다
+  });
+});
+// 로그인
+router.post('/light-sign-in', (req, res, next) => {
+  let paramL = JSON.parse(JSON.stringify(req.body));
+  let userID = paramL['userID'];
+  let userPW = paramL['userPW'];
+  db.lightloginCheck(userID, userPW, result => {
+    if (result.length > 0) {
+      res.redirect('/');
+    } else {
+      res.send(`<script>alert('로그인정보가 일치하지 않습니다'); document.location.href="/light-sign-in";</script>`);
+    }
+  });
+});
+
+// 회원가입 페이지
+router.post('/light-sign-up', (req, res, next) => {
+  let paramS = JSON.parse(JSON.stringify(req.body));
+  let userName = paramS['userName'];
+  let userID = paramS['userID'];
+  let userPW = paramS['userPW'];
+  db.lightuserSignUp(userName, userID, userPW, () => {
+    res.redirect('/light');
+    //past 에서 특정 값만 넘겨줄 때 row 대신 넘겨줄 값을 써준 후 <% row.id %> 가 아닌 <% id %> 를 써준다
+  });
+});
+
 module.exports = router;
